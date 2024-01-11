b29bb9a0cbd8d18b7a941d2b627302fb9956ffc3
ludiaz978 <142175671+ludiaz978@users.noreply.github.com>
2024-01-11 01:26:51 -0800

    API testing

--- client/src/components/StockRow.js
+++ client/src/components/StockRow.js
@@ -1,5 +1,30 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+//const axios = require('axios');
+// test
+const API_BASE_URL = 'https://api.polygon.io/v1/open-close'
+const API_KEY = '5xHHbQylCoepucqudYPs7vjYd9Meaiuq';
+const ticker = 'AAPL'
+const date = '2024-01-10'
+const fetchStockByTicker = async (ticker, date) => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/${ticker}/${date}`, {
+        params: {
+          apiKey: API_KEY
+        }
+      });
+  
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching StockData:", error);
+      throw error; 
+    }
+  }
 
+//   fetchStockByTicker(ticker, date)
+//     .then((data) => {
+//         console.log(data)
+//     })
 
 const StockRow = (props) => {
     //code state for sockdata
---
+++ client/src/components/polygon.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+//axios instance/ base url/ API key
+
+    API_BASE_URL: 'https://api.polygon.io/v1/open-close',
+      API_KEY: '5xHHbQylCoepucqudYPs7vjYd9Meaiuq',
+    
+
